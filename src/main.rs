b8a6ff2769bbddc3aa0ac0c7b31f04769d704cf6
gsproston <gsproston94@gmail.com>
2024-01-18 00:03:13 +0000

    Reading CSV from arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::error::Error;
+use csv::Reader;
+
+fn read_csv(csv_file_name: &String) -> Result<(), Box<dyn Error>> {
+    let mut rdr = Reader::from_path(csv_file_name)?;
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let csv_file_name = &args[1];
+    read_csv(csv_file_name);
 }
